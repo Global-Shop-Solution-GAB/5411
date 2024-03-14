e40430b5b278387cc922d17d31e6ddc00c3ddcef
Service Web <sw@gssmail.com>
2024-03-14 10:08:10 -0500

    Commit changes in C:\Websites\Git\emarlow\5411\GCG_5411_Maintenance.sql

---
+++ GCG_5411_Maintenance.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5411_Maintenance
+ * ========================================== */
+CREATE TABLE "GCG_5411_Maintenance"(
+ "ID" IDENTITY DEFAULT '0',
+ "SETTING" CHAR(5),
+ "VALUE" CHAR(75),
+ UNIQUE ("ID"));
+
+
