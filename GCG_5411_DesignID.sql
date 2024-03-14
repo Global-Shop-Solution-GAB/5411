62ed944a7f2b319926aecc99f39d510ea60a6662
Service Web <sw@gssmail.com>
2024-03-14 10:08:10 -0500

    Commit changes in C:\Websites\Git\emarlow\5411\GCG_5411_DesignID.sql

---
+++ GCG_5411_DesignID.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5411_DesignID
+ * ========================================== */
+CREATE TABLE "GCG_5411_DesignID"(
+ "ID" IDENTITY DEFAULT '0',
+ "DESIGN_ID" CHAR(50),
+ "NAME" CHAR(50),
+ UNIQUE ("ID"));
+
+
